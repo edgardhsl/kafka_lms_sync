df8ab66c3ea5de07885a3967f678237dc0e5fbc5
Edgard Hufelande <edgardhwp@outlook.com>
2023-04-27 22:59:39 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { config } from 'dotenv';
 config();
 
-const port = process.env.PORT || 2000;
+const port = process.env.PORT || 2222;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
